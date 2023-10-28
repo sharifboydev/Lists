00bb965a1cad150c0c8eba15d8e0f8e1a7934e08
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-10-28 10:45:08 +0500

    added new pythoncode

---
+++ list.py
@@ -0,0 +1,20 @@
+# mevalar = ["olma","banan","anjir","shaftoli"]
+# narhlar = [1200, 18000, 19000, 21000]
+# narhlar[0] = 13000
+# narhlar[2] = 29000
+# narhlar[3] = narhlar[3]+2000
+# mevalar.append("tarvuz")
+# sonlar = ["bir", "ikki", 3, 4, 5]
+# ismlar.append("javohir")
+# ismlar.append("azizbek")
+# print("Birinchi meva:", mevalar[0].title())
+# print("Ikkinchi meva:", mevalar[1].upper())
+# print(narhlar)
+# print(mevalar)
+# print(ismlar)
+# print(narhlar[2]+narhlar[3])
+
+
+# ismlar = ["javohir","hgyug", "iyguyg"]
+# ismlar.insert(0,'sas')
+# print(ismlar)
